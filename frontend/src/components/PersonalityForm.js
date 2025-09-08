66432718c565da2d29a86b4b6f6b67046a465c23
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-09-07 22:11:19 -0700

    Added introversion slider to PersonalityForm and added personality form to App.js

--- frontend/src/components/PersonalityForm.js
+++ frontend/src/components/PersonalityForm.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonalityForm = () => {
+  const [name, setName] = useState("");
+  const [introvert, setIntrovert] = useState(50);
+
   return (
     <div>
       <h2>Personality Form</h2>
-      <p>This is where the form will go</p>
+      <label>
+        Your Name:
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+
+      <div>
+        <label>
+          Introversion: {introvert}%
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={introvert}
+            onChange={(e) => setIntrovert(e.target.value)}
+          />
+        </label>
+      </div>
     </div>
   );
 };
