477530c8054f7441688dd1c7512f1eb2de5a7044
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-09-07 22:06:54 -0700

    Created PersonalityForm

---
+++ frontend/src/components/PersonalityForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PersonalityForm = () => {
+  return (
+    <div>
+      <h2>Personality Form</h2>
+      <p>This is where the form will go</p>
+    </div>
+  );
+};
+
+export default PersonalityForm;
