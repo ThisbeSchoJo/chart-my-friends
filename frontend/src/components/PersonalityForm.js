04fefba189b830554ec142e91f4e2ee13bf79223
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-09-07 22:15:13 -0700

    added inputs to the personality form

--- frontend/src/components/PersonalityForm.js
+++ frontend/src/components/PersonalityForm.js
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 const PersonalityForm = () => {
   const [name, setName] = useState("");
   const [introvert, setIntrovert] = useState(50);
+  const [intuitive, setIntuitive] = useState(50);
+  const [thinking, setThinking] = useState(50);
+  const [judging, setJudging] = useState(50);
 
   return (
     <div>
@@ -28,6 +31,49 @@ const PersonalityForm = () => {
             onChange={(e) => setIntrovert(e.target.value)}
           />
         </label>
+        <p>Extroversion: {100 - introvert}%</p>
+      </div>
+
+      <div>
+        <label>
+          Intuition: {intuitive}%
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={intuitive}
+            onChange={(e) => setIntuitive(e.target.value)}
+          />
+        </label>
+        <p>Sensing: {100 - intuitive}%</p>
+      </div>
+
+      <div>
+        <label>
+          Thinking: {thinking}%
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={thinking}
+            onChange={(e) => setThinking(e.target.value)}
+          />
+        </label>
+        <p>Feeling: {100 - thinking}%</p>
+      </div>
+
+      <div>
+        <label>
+          Judging: {judging}%
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={judging}
+            onChange={(e) => setJudging(e.target.value)}
+          />
+        </label>
+        <p>Perceiving: {100 - judging}%</p>
       </div>
     </div>
   );
